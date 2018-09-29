b22956b25e1f9b0e5a61b9f96cf0f4daf6eda06f
Dim <dtheoharis@gmail.com>
2018-09-30 00:30:29 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Dimitris Theoharis
+set -e
+
+CRON_SCHEDULE=${CRON_SCHEDULE:-50 23 * * *}
+export MONGO_HOST=${MONGO_HOST:-mongodb}
+export MONGO_PORT=${MONGO_PORT:-27017}
+
+if [[ "$1" == 'no-cron' ]]; then
+    exec /backup.sh
+else
+    LOGFIFO='/var/log/cron.fifo'
+    if [[ ! -e "$LOGFIFO" ]]; then
+        mkfifo "$LOGFIFO"
+    fi
+    CRON_ENV="MONGO_HOST='$MONGO_HOST'"
+    CRON_ENV="$CRON_ENV\nMONGO_PORT='$MONGO_PORT'"
+    echo -e "$CRON_ENV\n$CRON_SCHEDULE /backup.sh > $LOGFIFO 2>&1" | crontab -
+    crontab -l
+    cron
+    tail -f "$LOGFIFO"
+fi
